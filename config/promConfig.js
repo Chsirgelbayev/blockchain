d866887eecd70d226d2b83e8ab5dbce1c32b2b74
Chingiz <csirgilbaev@gmail.com>
2022-06-08 11:44:32 +0600

    Update promConfig, add prefix

--- config/promConfig.js
+++ config/promConfig.js
@@ -4,6 +4,7 @@ const promeConfig = {
     requestDurationBuckets: [0.1, 0.5, 1, 1.5],
     requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
     responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+		prefix: "blockchain_",
 };
 
 module.exports = promeConfig;
